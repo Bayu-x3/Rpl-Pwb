f99a94002950b89b625301e7a7cc85f83f0ba4da
Bayu Candra Yudistira <rpsmaster12@gmail.com>
2024-01-09 22:00:52 +0700

    Create index.php

---
+++ Php-Form-Bassic/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Media Online</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+   <style>
+      a {
+         text-decoration: none;
+      }
+   </style>
+</head>
+<body>
+   <h1>Media Online</h1>
+   <h2>Sosial Media Developer</h2>
+   <p>Belajar dan berbagi agar hidup menjadi lebih baik</p>
+   <h3>Benefit Join di Media Online</h3>
+   <nav>
+      <ul>
+         <li>Mendapatkan motivasi dari sesama para Developer</li>
+         <li>Sharing knowledge</li>
+         <li>Dibuat oleh canon web developer terbaik</li>
+      </ul>
+   </nav>
+
+   <h3>Cara Bergabung ke Media Online</h3>
+   <nav>
+      <ul>
+         <ol>
+            <li>Mengunjungi Website ini</li>
+            <li>Mendaftar di <a href="form.php">Form Sign Up</a></li>
+            <li>Mengunjungi Website ini</li>
+         </ol>
+      </ul>
+   </nav>
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
