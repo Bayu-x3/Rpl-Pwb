ce4ea0d8d7b54e59e0b9580eab3a49096e17d8b1
Bayu Candra Yudistira <rpsmaster12@gmail.com>
2024-01-16 15:32:42 +0700

    Update ape.php

--- PHP-OOP/Ape.php
+++ PHP-OOP/Ape.php
@@ -1,1 +1,15 @@
+  <?php
+  class Ape extends Animal
+  {
+      public function __construct($name, $legs = 2, $cold_blooded = "no")
+      {
+          $this->name = $name;
+          $this->legs = $legs;
+          $this->cold_blood = $cold_blooded;
+      }
 
+      public function yell()
+      {
+          echo "Auooo";
+      }
+  }
